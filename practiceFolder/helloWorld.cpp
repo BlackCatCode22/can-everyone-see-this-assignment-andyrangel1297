ca2f17707ff33d212a1ea8ceaa0f5489ecb6f152
Andy Rangel <77663935+andyrangel1297@users.noreply.github.com>
2024-01-18 01:24:57 -0800

    first commit - created practiceFolder

---
+++ practiceFolder/helloWorld.cpp
@@ -0,0 +1,18 @@
+// C++ program to display "Hello World"
+
+// Header file for input/output functions (cout and cin)
+#include <iostream>
+
+using namespace std;
+// let's you skip writing "std::" after every command
+
+// main() function is where the first program begins always
+int main() {
+    
+    // prints hello world
+    cout << "Hello, world!";
+
+    return 0;
+    // returns 0 to main function meaning everything worked (1 usally means error)
+    
+}
