b37867efe780f940f271d359e382a511ed3159f4
Andy Rangel <77663935+andyrangel1297@users.noreply.github.com>
2024-01-21 21:45:16 -0800

    added 3e Friendly Greeting HW

---
+++ 3e.cpp
@@ -0,0 +1,17 @@
+// "Friendly Greeting to User Assignment" - Andy Rangel
+// 01-21-2024
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "! I hope you have a very nice day today!" << endl;
+    
+    return 0;
+}
