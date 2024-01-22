0c4369b0fd40646fde89042cf6773efbb1bbbb21
Andy Rangel <77663935+andyrangel1297@users.noreply.github.com>
2024-01-21 21:39:22 -0800

    added 3b Double User Input HW

---
+++ 3b.cpp
@@ -0,0 +1,17 @@
+// "Doubling User Input Assignment" - Andy Rangel
+// 01/21/2024
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int userNum;
+
+    cout << "Enter a number: ";
+    cin >> userNum;
+
+    cout << userNum << " doubled is: " << userNum * 2 << endl;
+
+    return 0;
+}
