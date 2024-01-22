fd0e4bb637d5bbfeaefadf8551e76998d1fff9db
Andy Rangel <77663935+andyrangel1297@users.noreply.github.com>
2024-01-21 21:42:23 -0800

    added 3c Triple User Input HW

---
+++ 3c.cpp
@@ -0,0 +1,18 @@
+// "Tripling User Input Assignment" - Andy Rangel
+// 01-21-2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int userNum;
+
+    cout << "Enter a number: ";
+    cin >> userNum;
+
+    cout << userNum << " tripled is: " << userNum * 3 << endl;
+
+    return 0;
+}
