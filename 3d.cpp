0b7964b145da07e8e203cf0f96afa6c0e7ec8537
Andy Rangel <77663935+andyrangel1297@users.noreply.github.com>
2024-01-21 21:48:14 -0800

    added 3d Adding User Nums HW

---
+++ 3d.cpp
@@ -0,0 +1,19 @@
+// "Adding User Inputs Assignment" - Andy Rangel
+// 01-21-2024
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "The sum of the two numbers is: " << num1 + num2 << endl;
+
+    return 0;
+}
